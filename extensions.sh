be7d2cda4a561b0ccd22ea545c664fefef7dd498
Vladimir <109805456+eureves@users.noreply.github.com>
2024-01-28 18:40:05 +0300

    Create extensions.sh

---
+++ extensions.sh
@@ -0,0 +1,15 @@
+code --install-extension Angular.ng-template
+code --install-extension Anksio.tsoding-emacs-gruber-minimal
+code --install-extension BugFrag.bugfrag
+code --install-extension christian-kohler.path-intellisense
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension EditorConfig.EditorConfig
+code --install-extension esbenp.prettier-vscode
+code --install-extension ghaschel.vscode-angular-html
+code --install-extension johnpapa.Angular2
+code --install-extension mhutchie.git-graph
+code --install-extension PKief.material-icon-theme
+code --install-extension segerdekort.angular-cli
+code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension streetsidesoftware.code-spell-checker-russian
+code --install-extension vscodevim.vim
